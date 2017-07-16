51620de840744a0b8dd6a0af8cc50343071d2b99
red-dylan <dylansherwood@gmail.com>
2017-07-16 09:18:06 -0400

    remove lazy load

--- portfolio-page/scripts/script.js
+++
@@ -1,5 +0,0 @@
-	$(".img-highres").off().on("load", function() {
-		var highres = $(this).attr("src").toString();
-		console.log(highres);
-		$("#top-background").css("background-image", "url(../" + highres +")");
-	});
